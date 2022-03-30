2da18f7410bf5f668f2f030e29afb55116023665
dusanivkovic <ivdule77@gmail.com>
2022-03-30 22:09:57 +0200

    Adding ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,10 @@
+function main(){
+    //only change code below this line
+    
+    //only change code abowe this line
+    //one row coment
+/*coment*/
+}
+console.log(main());
+
+module.exports = main;
